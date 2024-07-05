1e71a2b15f755bd073d6ebcf5e75016a3112c177
Guilherme Giusti <guilherme.giusti04@gmail.com>
2024-07-05 12:00:30 -0300

    removendo parametro

--- pkg/migration/migration.go
+++ pkg/migration/migration.go
@@ -24,8 +24,7 @@ func RunMigrations(dbInstance *gorm.DB, migrations []*gormigrate.Migration) {
 }
 
 // ExecRaw simplify error handling for sql commands
-func ExecRaw(db *gorm.DB, command string, version string) bool {
-	log.Printf("Executing Migration Versionament: %s", version)
+func ExecRaw(db *gorm.DB, command string) bool {
 	err := db.Exec(command).Error
 	if err != nil {
 		message := fmt.Sprintf("Migration Error into %v environment while evaluating command:\n\n ==> %v \n\nERROR:\n\n ==> %v", os.Getenv("ENVIRONMENT"), command, err.Error())
